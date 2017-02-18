a3c7dfb8073b11e850ed9ad90c609f43a0889d89
Franklin Jeng <franklinjjeng@gmail.com>
2017-02-18 11:51:48 -0800

    (feat) NodeJS-git-server attempted start

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,31 @@ var express = require('express');
 var parser = require('body-parser');
 var mysql = require('mysql');
 
+/******** TEST CODE ***********/
+var GitServer = require('git-server');
+
+var newUser = {
+    username:'demo',
+    password:'demo'
+}
+var newRepo = {
+    name:'asdf',
+    anonRead:false,
+    users: [
+        { user:newUser, permissions:['R','W'] }
+    ]
+}
+server = new GitServer([ newRepo ]);
+// server.on('commit', function(update, repo) {
+//     // do some logging or other stuff
+//     update.accept() //accept the update.
+// });
+// server.on('post-update', function(update, repo) {
+//     //do some deploy stuff
+// });
+
+/******** TEST CODE ***********/
+
 var path = require('path');
 var schema = require('../db/schema.js')
 
