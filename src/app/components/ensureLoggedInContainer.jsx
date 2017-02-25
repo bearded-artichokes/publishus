a62b4809b80eaa2e3d3a4ac21b5b024bbf009c41
Abiy Melaku <agirma08@gmail.com>
2017-02-25 15:24:25 -0800

    Remove console.log

--- src/app/components/ensureLoggedInContainer.jsx
+++ src/app/components/ensureLoggedInContainer.jsx
@@ -13,10 +13,7 @@ import * as user from '../actions/userActions.jsx';
 
 export default class EnsureLoggedInContainer extends React.Component {
   componentWillMount() {
-    console.log('IN LOGGED IN CONTAINER');
     if (!this.props.isLoggedIn) {
-      console.log('!this.props.isLoggedIn', !this.props.isLoggedIn);
-      console.log('this.props.location.pathname', this.props.location.pathname);
       this.props.dispatch(user.setRedirectUrl(this.props.location.pathname));
       // this.props.router.push('/login');
     }
